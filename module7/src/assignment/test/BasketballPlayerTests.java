b52f46bc270489b5e8076ae8be1ccd3a03a1bf12
DiscoBroccoli <19515DearClem@gmail.com>
2024-04-08 15:09:42 -0500

    assignment 7 submit

--- module7/src/assignment/test/BasketballPlayerTests.java
+++ module7/src/assignment/test/BasketballPlayerTests.java
@@ -1,25 +1,115 @@
 package assignment.test;
 
-import java.util.Arrays;
-
 public class BasketballPlayerTests {
     public static void main(String[] args) {
-        // Test Unit: HockeyPlayer(String, int)
-        System.out.println("HockeyPlayer(String, int)");
-        // Test #1 - Initialize percentages array without data.
-/*        System.out.println("Test #1 - Initialize the name.");
-        BasketballPlayer basketballPlayer = new BasketballPlayer();
-        System.out.println("Expected: [0, 0, 0, 0]");
-        System.out.println("Actual: " + Arrays.toString(teamShotOnGoalPercentages.getPercentages()));
+        // Test Unit: BasketballPlayer(String, int)
+        System.out.println("BasketballPlayer(String, int)");
+        // Test #1 - Initialize the name.
+        System.out.println("Test #1 - Initialize the name.");
+        BasketballPlayer basketballPlayer = new BasketballPlayer("John Smith", 8);
+        System.out.println("Expected: John Smith");
+        System.out.println("Actual: " + basketballPlayer.getName());
         System.out.println();
 
-        // Test Unit: TeamShotOnGoalPercentages(double[])
-        System.out.println("TeamShotOnGoalPercentages(double[])");
-        // Test #1 - Initialize percentages array without data.
-        System.out.println("Test #1 - Initialize percentages array with data.");
-        teamShotOnGoalPercentages = new TeamShotOnGoalPercentages(new double[]{0.1, 0.5, 0.6, 0.3});
-        System.out.println("Expected: [0.1, 0.5, 0.6, 0.3]");
-        System.out.println("Actual: " + Arrays.toString(teamShotOnGoalPercentages.getPercentages()));
-        System.out.println();*/
+        // Test #2 - Initialize the number.
+        System.out.println("Test #2 - Initialize the number.");
+        System.out.println("Expected: 8");
+        System.out.println("Actual: " + basketballPlayer.getNumber());
+        System.out.println();
+
+        // Test #3 - Initialize the free throws.
+        System.out.println("Test #3 - Initialize the free throws.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        System.out.println("Expected: 0");
+        System.out.println("Actual: " + basketballPlayer.getFreeThrows());
+        System.out.println();
+
+        // Test #4 - Initialize the field goals.
+        System.out.println("Test #4 - Initialize the field goals.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        System.out.println("Expected: 0");
+        System.out.println("Actual: " + basketballPlayer.getFieldGoals());
+        System.out.println();
+
+        // Test #5 - Initialize the three pointers.
+        System.out.println("Test #4 - Initialize the three pointers.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        System.out.println("Expected: 0");
+        System.out.println("Actual: " + basketballPlayer.getThreePointers());
+        System.out.println();
+
+        // Test Unit: setName(String) : void
+        System.out.println("setName(String) : void");
+        // Test #1 - Updates the state of name.
+        System.out.println("Test #1 - Updates the state of name.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        basketballPlayer.setName("John Snow");
+        System.out.println("Expected: John Snow");
+        System.out.println("Actual: " + basketballPlayer.getName());
+        System.out.println();
+
+        // Test Unit: setNumber(int) : void
+        System.out.println("setNumber(int) : void");
+        // Test #1 - Updates the state of number.
+        System.out.println("Test #1 - Updates the state of number.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        basketballPlayer.setNumber(10);
+        System.out.println("Expected: 10");
+        System.out.println("Actual: " + basketballPlayer.getNumber());
+        System.out.println();
+
+        // Test Unit: setFreeThrows(int) : void
+        System.out.println("setFreeThrows(int) : void");
+        // Test #1 - Updates the state of free throws.
+        System.out.println("Test #1 - Updates the state of free throws.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        basketballPlayer.setFreeThrows(10);
+        System.out.println("Expected: 10");
+        System.out.println("Actual: " + basketballPlayer.getFreeThrows());
+        System.out.println();
+
+        // Test Unit: setFieldGoals(int) : void
+        System.out.println("setFieldGoals(int) : void");
+        // Test #1 - Updates the state of field goals.
+        System.out.println("Test #1 - Updates the state of field goals.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        basketballPlayer.setFieldGoals(7);
+        System.out.println("Expected: 7");
+        System.out.println("Actual: " + basketballPlayer.getFieldGoals());
+        System.out.println();
+
+        // Test Unit: setThreePointers(int) : void
+        System.out.println("setThreePointers(int) : void");
+        // Test #1 - Updates the state of three pointers.
+        System.out.println("Test #1 - Updates the state of three pointers.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        basketballPlayer.setThreePointers(9);
+        System.out.println("Expected: 9");
+        System.out.println("Actual: " + basketballPlayer.getThreePointers());
+        System.out.println();
+
+        // Test Unit: getPoints() : int
+        System.out.println("getPoints() : int");
+        // Test #1 - Updates the state of three pointers.
+        System.out.println("Test #1 - Updates the state of three pointers.");
+        basketballPlayer = new BasketballPlayer("John Smith", 8);
+        basketballPlayer.setFreeThrows(8);
+        basketballPlayer.setFieldGoals(5);
+        basketballPlayer.setThreePointers(7);
+        System.out.println("Expected: 39");
+        System.out.println("Actual: " + basketballPlayer.getPoints());
+        System.out.println();
+
+        // Test Unit: toString() : String
+        System.out.println("toString() : String");
+        // Test #1 - Returns the correct String representation.
+        System.out.println("Test #1 - Returns the correct String representation.");
+        basketballPlayer = new BasketballPlayer("John Snow", 8);
+        basketballPlayer.setFreeThrows(8);
+        basketballPlayer.setFieldGoals(5);
+        basketballPlayer.setThreePointers(7);
+        System.out.println("Expected: Basketball Player: John Snow [8], Points: 39");
+        System.out.println("Actual: " + basketballPlayer);
+        System.out.println();
     }
 }
--- module7/src/assignment/test/HockeyPlayerTests.java
+++ module7/src/assignment/test/HockeyPlayerTests.java
@@ -1,7 +1,5 @@
 package assignment.test;
 
-import java.util.Arrays;
-
 public class HockeyPlayerTests {
     public static void main(String[] args) {
         // Test Unit: HockeyPlayer(String, int)
@@ -48,14 +46,74 @@ public class HockeyPlayerTests {
 
         // Test #3 - Initialize the goals.
         System.out.println("Test #3 - Initialize the goals.");
-        System.out.println("Expected: 0");
+        System.out.println("Expected: 5");
         System.out.println("Actual: " + hockeyPlayer.getGoals());
         System.out.println();
 
         // Test #4 - Initialize the assists.
         System.out.println("Test #4 - Initialize the assists.");
-        System.out.println("Expected: 0");
+        System.out.println("Expected: 7");
         System.out.println("Actual: " + hockeyPlayer.getAssists());
+        System.out.println();
+
+        // Test Unit: Player Class Members | getName() : String
+        System.out.println("Tests for Player Class Members");
+        System.out.println("getName() : String");
+        // Test #1 - setName(String) : void
+        System.out.println("Test #1 - Updates the state of name.");
+        hockeyPlayer = new HockeyPlayer("John Smith", 8);
+        hockeyPlayer.setName("John Snow");
+        System.out.println("Expected: John Snow");
+        System.out.println("Actual: " + hockeyPlayer.getName());
+        System.out.println();
+
+        // Test Unit: setNumber(int) : void
+        System.out.println("setNumber(int) : void");
+        // Test #1 - setName(String) : void
+        System.out.println("Test #1 - Updates the state of number.");
+        hockeyPlayer = new HockeyPlayer("John Smith", 8);
+        hockeyPlayer.setNumber(10);
+        System.out.println("Expected: 10");
+        System.out.println("Actual: " + hockeyPlayer.getNumber());
+        System.out.println();
+
+        // Test Unit: setGoals(int) : void
+        System.out.println("setGoals(int) : void");
+        // Test #1 - Updates the state of goals.
+        System.out.println("Test #1 - Updates the state of goals.");
+        hockeyPlayer = new HockeyPlayer("John Smith", 8);
+        hockeyPlayer.setNumber(10);
+        System.out.println("Expected: 10");
+        System.out.println("Actual: " + hockeyPlayer.getNumber());
+        System.out.println();
+
+        // Test Unit: setAssists(int) : void
+        System.out.println("setAssists(int) : void");
+        // Test #1 - Updates the state of assists.
+        System.out.println("Test #1 - Updates the state of assists.");
+        hockeyPlayer = new HockeyPlayer("John Smith", 8);
+        hockeyPlayer.setAssists(8);
+        hockeyPlayer.setAssists(10);
+        System.out.println("Expected: 10");
+        System.out.println("Actual: " + hockeyPlayer.getAssists());
+        System.out.println();
+
+        // Test Unit: getPoints() : int
+        System.out.println("getPoints() : int");
+        // Test #1 - Returns the correct number of points.
+        System.out.println("Test #1 - Returns the correct number of points.");
+        hockeyPlayer = new HockeyPlayer("John Smith", 8, 5, 7);
+        System.out.println("Expected: 12");
+        System.out.println("Actual: " + hockeyPlayer.getPoints());
+        System.out.println();
+
+        // Test Unit: toString() : String
+        System.out.println("toString() : String");
+        // Test #1 - Returns the correct String representation.
+        System.out.println("Test #1 - Returns the correct String representation.");
+        hockeyPlayer = new HockeyPlayer("John Snow", 8, 5, 7);
+        System.out.println("Expected: Hockey Player - John Snow [8], Points: 12");
+        System.out.println("Actual: " + hockeyPlayer);
         System.out.println();
     }
 }
