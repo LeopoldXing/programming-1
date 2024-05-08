8e59a922a1c77179b64b7619317db5b6db32668d
DiscoBroccoli <19515DearClem@gmail.com>
2024-05-07 21:32:23 -0500

    test for gpg on windows

--- module7/src/Game.java
+++ module7/src/Game.java
@@ -1,24 +1,19 @@
-public abstract class Game
-{
+public abstract class Game {
     private String description;
 
-    public Game(String description)
-    {
+    public Game(String description) {
         this.setDescription(description);
     }
 
-    public String getDescription()
-    {
+    public String getDescription() {
         return this.description;
     }
 
-    public void setDescription(String description)
-    {
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public String toString()
-    {
+    public String toString() {
         return String.format("Description: %s", this.description);
     }
 }
